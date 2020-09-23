7e20947f8456b85b060324877cb6a2c4011bd67c
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-23 11:46:02 +0200

    validated Court

--- HtkTennis.Entities/Models/Court.cs
+++ HtkTennis.Entities/Models/Court.cs
@@ -1,18 +1,78 @@
-﻿using System;
+﻿using HtkTennis.Utilities;
+
+using System;
 using System.Collections.Generic;
 
 namespace HtkTennis.Entities
 {
     public partial class Court
     {
+        #region
+        private int pkCourtId;
+        private string courtName;
+        #endregion
+
         public Court()
         {
             Reservations = new HashSet<Reservation>();
         }
 
-        public int PkCourtId { get; set; }
-        public string CourtName { get; set; }
+        #region Properties
+        /// <summary>
+        /// Id of the <see cref="Court"/>
+        /// </summary>
+        public int PkCourtId
+        {
+            get
+            {
+                return pkCourtId;
+            }
+            set
+            {
+                if(pkCourtId != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        pkCourtId = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(pkCourtId));
+                    }
+                }
+            }
+        }
 
+        /// <summary>
+        /// Name of the <see cref"Court"/>
+        /// </summary>
+        public string CourtName
+        {
+            get
+            {
+                return courtName;
+            }
+            set
+            {
+                if(courtName != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsStringNull(value);
+                    if(isValid)
+                    {
+                        courtName = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(courtName));
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Navigation Properties
         public virtual ICollection<Reservation> Reservations { get; set; }
+        #endregion
     }
 }
--- HtkTennis.Utilities/Validations.cs
+++ HtkTennis.Utilities/Validations.cs
@@ -153,5 +153,4 @@ namespace HtkTennis.Utilities
         }
         #endregion
     }
-
 }
