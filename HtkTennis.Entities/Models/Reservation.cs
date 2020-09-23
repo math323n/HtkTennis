a6f8046cdebb3cbd8539d9e007d51ddb775b04f5
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-23 11:20:47 +0200

    Improving Validation on Reservation Entity

--- HtkTennis.Entities/Models/Reservation.cs
+++ HtkTennis.Entities/Models/Reservation.cs
@@ -1,14 +1,115 @@
-﻿using System;
+﻿using HtkTennis.Utilities;
+
+using System;
 using System.Collections.Generic;
 
 namespace HtkTennis.Entities
 {
     public partial class Reservation
     {
-        public int PkReservationId { get; set; }
-        public int FkCourtId { get; set; }
-        public int FkFirstMemberId { get; set; }
-        public int FkSecondMemberId { get; set; }
+        #region Fields
+        private int pkReservationId;
+        private int fkCourtId;
+        private int fkFirstMemberId;
+        private int fkSecondMemberId;
+        #endregion
+
+        public int PkReservationId
+        {
+            get
+            {
+                return pkReservationId;
+            }
+            set
+            {
+                if(pkReservationId != value)
+                {
+                    // Using the validation class, check if the value does not go below 0
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        pkReservationId = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(PkReservationId));
+                    }
+                }
+            }
+        }
+
+        public int FkCourtId
+        {
+            get
+            {
+                return fkCourtId;
+            }
+            set
+            {
+                if(fkCourtId != value)
+                {
+                    // Using the validation class, check if the value does not go below 0
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        fkCourtId = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(FkCourtId));
+                    }
+                }
+            }
+        }
+
+        public int FkFirstMemberId
+        {
+            get
+            {
+                return fkFirstMemberId;
+            }
+            set
+            {
+                if(fkFirstMemberId != value)
+                {
+                    // Using the validation class, check if the value does not go below 0
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        fkFirstMemberId = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(FkFirstMemberId));
+                    }
+                }
+            }
+        }
+
+
+        public int FkSecondMemberId
+        {
+            get
+            {
+                return fkSecondMemberId;
+            }
+            set
+            {
+                if(fkSecondMemberId != value)
+                {
+                    // Using the validation class, check if the value does not go below 0
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        fkSecondMemberId = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(fkSecondMemberId));
+                    }
+                }
+            }
+        }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
 
---
+++ HtkTennis.GUI/ViewModels/CourtViewModel.cs
@@ -0,0 +1,53 @@
+﻿using HtkTennis.Entities;
+using HtkTennisGui.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace HtkTennis.GUI.ViewModels
+{
+    public class CourtViewModel: ViewModelBase
+    {
+        #region Fields
+        protected ObservableCollection<Member> courts;
+        protected Court selectedCourt;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor for MemberViewModel
+        /// </summary>
+        public CourtViewModel()
+        {
+            Courts = new ObservableCollection<Court>();
+        }
+        #endregion
+
+        #region Properties
+        public virtual ObservableCollection<Member> Members
+        {
+            get
+            {
+                return courts;
+            }
+            set
+            {
+                SetProperty(ref courts, value);
+            }
+        }
+
+        public virtual Court SelectedMember
+        {
+            get
+            {
+                return selectedCourt;
+            }
+            set
+            {
+                SetProperty(ref selectedCourt, value);
+            }
+        }
+        #endregion
+    }
+}
