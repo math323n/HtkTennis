2a0f1cd98fc7278dbc15f2daff5e615043215677
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-23 11:50:21 +0200

    Added validation of Ranking

--- HtkTennis.Entities/Models/Ranking.cs
+++ HtkTennis.Entities/Models/Ranking.cs
@@ -1,14 +1,99 @@
-﻿using System;
+﻿using HtkTennis.Utilities;
+using System;
 using System.Collections.Generic;
 
 namespace HtkTennis.Entities
 {
     public partial class Ranking
     {
-        public int PkRankId { get; set; }
-        public int FkMemberId { get; set; }
-        public int Points { get; set; }
+        #region Fields
+        private int pkRankId;
+        private int fkMemberId;
+        private int points;
+        #endregion
 
+        #region Properties
+        /// <summary>
+        /// Id of the case <see cref="Ranking"/>
+        /// </summary>
+        public int PkRankId
+        {
+            get
+            {
+                return pkRankId;
+            }
+            set
+            {
+                if(pkRankId != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        pkRankId = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(pkRankId));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Id of the <see cref="Member"/>
+        /// </summary>
+        public int FkMemberId
+        {
+            get
+            {
+                return fkMemberId;
+            }
+            set
+            {
+                if(fkMemberId != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        fkMemberId = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(fkMemberId));
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Points of the <see cref="Ranking"/>
+        /// </summary>
+        public int Points
+        {
+            get
+            {
+                return points;
+            }
+            set
+            {
+                if(points != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        points = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(points));
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Navigation Properties
         public virtual Member FkMember { get; set; }
+        #endregion
+
     }
 }
