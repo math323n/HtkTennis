6ccf47cad66a7ee5fc2994a890fca7cc80d8b232
Troll Coder <math323n@edu.campusvejle.dk>
2020-09-24 08:39:55 +0200

    Edited a viewmodel

--- HtkTennis.GUI/ViewModels/ReservationViewModel.cs
+++ HtkTennis.GUI/ViewModels/ReservationViewModel.cs
@@ -1,20 +1,37 @@
-﻿using HtkTennis.Entities;
+﻿using HtkTennis.DataAccess.Base;
+using HtkTennis.DataAccess.Factory;
+using HtkTennis.DataAccess.Repositories;
+using HtkTennis.Entities;
+using HtkTennis.Utilities;
+
 using HtkTennisGui.ViewModels.Base;
+
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace HtkTennis.GUI.ViewModels
 {
     public class ReservationViewModel: ViewModelBase
     {
+        #region Properties
         protected ObservableCollection<Reservation> reservations;
         protected Reservation selectedReservation;
+        #endregion
 
+        #region Constructor
         public ReservationViewModel()
         {
-            Reservations = new ObservableCollection<Reservation>();
+            // Initialize collection to prevent error with the ReplaceWith extention method
+            reservations = new ObservableCollection<Reservation>();
         }
+        #endregion
 
-        public virtual ObservableCollection<Reservation> Reservations
+        #region Properties
+        /// <summary>
+        /// The displayed reservations in the view
+        /// </summary>
+        public ObservableCollection<Reservation> Reservations
         {
             get
             {
@@ -26,7 +43,10 @@ namespace HtkTennis.GUI.ViewModels
             }
         }
 
-        public virtual Reservation SelectedReservation
+        /// <summary>
+        /// The selected reservation in the view
+        /// </summary>
+        public Reservation SelectedReservation
         {
             get
             {
@@ -37,5 +57,20 @@ namespace HtkTennis.GUI.ViewModels
                 SetProperty(ref selectedReservation, value);
             }
         }
+        #endregion
+
+        #region Methods
+        protected override async Task LoadAllAsync()
+        {
+            // Create factory, and get the instance
+            RepositoryFactory<ReservationRepository, Reservation> reservationFactory = RepositoryFactory<ReservationRepository, Reservation>.GetInstance();
+            // Create repository with the factory
+            ReservationRepository reservationRepository = reservationFactory.Create();
+            // Get all reservations
+            IEnumerable<Reservation> reservations = await reservationRepository.GetAllAsync();
+            // Replace collection
+            Reservations.ReplaceWith(reservations);
+        }
+        #endregion
     }
 }
